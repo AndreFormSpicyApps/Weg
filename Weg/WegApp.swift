532089ca19dde7d5718b13015eb145f3195cb257
Andre Saubert <andre@spicyapps.com>
2022-11-25 16:04:09 +0100

    Initial Commit

---
+++ Weg/WegApp.swift
@@ -0,0 +1,17 @@
+//
+//  WegApp.swift
+//  Weg
+//
+//  Created by Andre on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
